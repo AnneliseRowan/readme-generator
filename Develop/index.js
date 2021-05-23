e1df28ebbe9541f26af3299067756f09083986a0
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-05-23 14:38:19 -0400

    fixed the bug as to why everything was saying undefined

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,44 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer"); 
 const fs = require("fs"); 
-const questions = require("./questions");
-const badgeIcon = require("./badges"); 
-const licenseText = require("./licenses"); 
+const questions = require("./questions.js");
+const badgeIcon = require("./badges.js"); 
+const licenseText = require("./licenses.js"); 
 let badge; 
 
-// TODO: Create an array of questions for user input
+const generateHTML = (replies) =>
+`# Table of Contents:
+* [Title](#Title)
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contribution](#Contribution)
+* [Tests](#Tests)
+* [License](#License)
+* [Github](#Github)
+* [Email](#Email)
+# Title:
+# ${replies.title}
+${badge}
+# Owned By:
+${replies.owner}
+## Description:
+${replies.description}
+# Installation
+${replies.installation}
+#Usage
+${replies.usage}
+#Contribution Guidelines
+${replies.contribution}
+#Tests
+Some of the tests that I ran for this project are: ${replies.tests}.
+#License
+${replies.license}
+#Github
+Please visit my ${replies.username} to visit my work.
+#Email
+My Email is ${replies.email}. Feel free to reach me here.`;
+
 inquirer
     .prompt(questions)
     .then((replies) => {
@@ -35,48 +67,5 @@ inquirer
         );
     }); 
 
-// TODO: Create a function to write README file
-const generateHTML = (replies) =>
-`# Table of Contents:
-* [Title](#Title)
-* [Description](#Description)
-* [Installation](#Installation)
-* [Usage](#Usage)
-* [Contribution](#Contribution)
-* [Tests](#Tests)
-* [License](#License)
-* [Github](#Github)
-* [Email](#Email)
-# Title:
-# ${replies.title}
-${badge}
-# Owned By:
-${replies.name}
-## Description:
-${replies.description}
-# Installation
-${replies.installation}
-#Usage
-${replies.usage}
-#Contribution Guidelines
-${replies.contribution}
-#Tests
-Some of the tests that I ran for this project are: ${replies.tests}.
-#License
-${replies.license}
-#Github
-Please visit my ${replies.username} to visit my work.
-#Email
-My Email is ${replies.email}. Feel free to reach me here.`;
 
 
-
-// function writeToFile(fileName, data) {
-
-// }
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
---
+++ Develop/readMe.md
@@ -0,0 +1,42 @@
+# Table of Contents:
+* [Title](#Title)
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contribution](#Contribution)
+* [Tests](#Tests)
+* [License](#License)
+* [Github](#Github)
+* [Email](#Email)
+# Title:
+# whoohoo
+[![ISC License](https://img.shields.io/badge/License-ISC-blue.svg)]
+# Owned By:
+undefined
+## Description:
+whooohoo
+# Installation
+whoohoo
+#Usage
+whoohoo
+#Contribution Guidelines
+whoohoo
+#Tests
+Some of the tests that I ran for this project are: undefined.
+#License
+ISC License
+Copyright (c) 2021
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#Github
+Please visit my whoohoo to visit my work.
+#Email
+My Email is whoohoo. Feel free to reach me here.
