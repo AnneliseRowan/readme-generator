957fff1395e46609b0a1f33987577913dcbe816a
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-05-23 10:41:24 -0400

    made another js file, made switch statement for licenses

--- Develop/index.js
+++ Develop/index.js
@@ -1,65 +1,73 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer"); 
 const fs = require("fs"); 
+const questions = require("./questions.js");
 
 // TODO: Create an array of questions for user input
 inquirer
-    .prompt([
-   {
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your project?',
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: 'Where is a description of you project?',
-  },
-  {
-    type: 'input',
-    name: 'installation',
-    message: 'What are the installation instructions?',
-  },
-  {
-    type: 'input',
-    name: 'usage',
-    message: `What is your project's usage information?`,
-  },
-  {
-    type: 'input',
-    name: 'contribution',
-    message: "What are the contribution guidelines?",
-  },
-  {
-    type: 'input',
-    name: 'test',
-    message: 'What are the test instructions?',
-  },
-  {
-    type: 'list',
-    name: 'license',
-    message: 'What license would you like?',
-    choices: ["BSD", "MIT", "GPL"],
-  },
-  {
-    type: 'input',
-    name: 'username',
-    message: 'What is your Github username?',
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'What is your email address?',
-  },
-])
-.then((replies) => {
-    switch(replies.license) {
-        case "MIT": 
-            r
-    }
-})
+    .prompt(questions)
+    .then((replies) => {
+        switch(replies.license) {
+            case "MIT" :
+                replies.license = licenseText.mit; 
+                badge = badgeIcon.mit
+                break; 
+            case "Apache" :
+                replies.license = licenseText.apache; 
+                badge = badgeIcon.apache
+                break;
+            case "GPL" :
+                replies.license = licenseText.gpl; 
+                badge = badgeIcon.gpl
+                break; 
+            case "ISC" :
+                replies.license = licenseText.isc;
+                badge = badgeIcon.isc
+                break; 
+        }
+        const file = "readMe.md";
+
+        fs.writeFile(file, generateHTML(replies), (err) => 
+        err ? console.log(err) : console.log("success!!!!whoohoo!!!")
+        );
+    }); 
 
 // TODO: Create a function to write README file
+const generateHTML = (replies) =>
+`# Table of Contents:
+* [Title](#Title)
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contribution](#Contribution)
+* [Tests](#Tests)
+* [License](#License)
+* [Github](#Github)
+* [Email](#Email)
+# Title:
+# ${replies.title}
+${badge}
+# Owned By:
+${replies.name}
+## Description:
+${replies.description}
+# Installation
+${replies.installation}
+#Usage
+${replies.usage}
+#Contribution Guidelines
+${replies.contribution}
+#Tests
+Some of the tests that I ran for this project are: ${replies.tests}.
+#License
+${replies.license}
+#Github
+Please visit my ${replies.username} to visit my work.
+#Email
+My Email is ${replies.email}. Feel free to reach me here.`;
+
+
+
 function writeToFile(fileName, data) {
 
 }
---
+++ Develop/questions.js
@@ -0,0 +1,53 @@
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'owner',
+        message: 'What is your name?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Where is a description of you project?',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: `What is your project's usage information?`,
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: "What are the contribution guidelines?",
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'What are the test instructions?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What license would you like?',
+        choices: ["Apache", "MIT", "GPL", "ISC"],
+      },
+      {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+]
