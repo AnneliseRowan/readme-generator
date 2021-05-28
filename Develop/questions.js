22da77f45eb24d063598ff6431c538f5792e201e
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-05-28 14:17:55 -0400

    tweaked the questions a bit

--- Develop/questions.js
+++ Develop/questions.js
@@ -22,10 +22,8 @@ module.exports = [
       {
         type: 'input',
         name: 'usage',
-        message: `Provide instructions and examples for use. Include screenshots as needed. To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-                ```md
-                ![alt text](assets/images/screenshot.png)
-                ````,
+        message: `Provide instructions and examples for use. Include screenshots as needed. To add a screenshot, create an "assets/images" folder in your repository and upload your screenshot to it. 
+        Then, using the relative filepath, add it to your README using the following syntax:"![alt text](assets/images/screenshot.png)"`,
       },
       {
         type: 'input',
